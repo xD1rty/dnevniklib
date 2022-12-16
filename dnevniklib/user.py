31c2501178f0c97b3886d7a3aed80493fa0bbf4f
Иван Кривенцев <dirtyhornet277@outlook.com>
2022-12-16 21:53:02 +0300

    Добавлена функция получения новых уведомлений

--- dnevniklib/user.py
+++ dnevniklib/user.py
@@ -5,9 +5,11 @@ from dnevniklib.errors import DnevnikLibError
 class User:
     def __init__(self, token=None) -> None:
         # Init function
+        self.default_api_url = "https://dnevnik.mos.ru/mobile/api"
         self.token = token
         self.session = Session()
         self.data_about_user = self.login()
+
         if self.data_about_user[1] == 200:
             self.data_about_user = self.data_about_user[0]
             self.first_name = self.data_about_user["profile"]["first_name"]
@@ -36,7 +38,7 @@ class User:
         :return:
         """
         result = self.session.get(
-            "https://dnevnik.mos.ru/mobile/api/profile", 
+            f"{self.default_api_url}/profile",
             headers={
                 "Authorization": self.token,
                 "Auth-Token": self.token
@@ -47,19 +49,19 @@ class User:
         if result.json():
             return [result.json(), result.status_code]
 
-    def get_date_in_format(self, year, month, date) -> str:
+    def get_date_in_format(self, year, month, day) -> str:
         """
 
         :param year:
         :param month:
-        :param date:
+        :param day:
         :return:
         """
-        if len(str(date)) == 1:
-            date ="0"+str(date)
+        if len(str(day)) == 1:
+            date ="0"+str(day)
         if len(str(month)) == 1:
             month ="0"+str(month)
-        return f"{year}-{month}-{date}"
+        return f"{year}-{month}-{day}"
 
     def get_attendance_by_date(self, to_date, from_date):
         """
@@ -69,11 +71,23 @@ class User:
         :return:
         """
         data = self.session.get(
-            f"https://dnevnik.mos.ru/mobile/api/visits?from={from_date}&to={to_date}&contract_id={self.contract_id}",
+            f"{self.default_api_url}/visits?from={from_date}&to={to_date}&contract_id={self.contract_id}",
             headers={
                 "Authorization": self.token,
                 "Auth-Token": self.token
             }
         )
         return data.json()["payload"]
+    def get_notifications(self):
+        """
+
+        :return:
+        """
+        return self.session.get(
+            f"{self.default_api_url}/notifications/search?student_id={self.id}",
+            headers={
+                "Authorization": self.token,
+                "Auth-Token": self.token
+            }
+        ).json()
         
