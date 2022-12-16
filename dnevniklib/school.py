f38ad4b06921a1b11181d1f1d71c2dcc1318d58d
Иван Кривенцев <dirtyhornet277@outlook.com>
2022-12-16 19:30:28 +0300

    Незначительные для пользователя, но для оптимизации важные изменения

--- dnevniklib/school.py
+++ dnevniklib/school.py
@@ -1,15 +1,9 @@
 class School:
-    def __init__(self, session, token) -> None:
+    def __init__(self, session, token, data_about_user) -> None:
         self.session = session
         self.token = token
-        
+        self.data_about_user = data_about_user
     def get_info_about_school(self):
-        data = self.session.get(
-            "https://dnevnik.mos.ru/mobile/api/profile",
-            headers={
-                "Authorization": self.token,
-                "Auth-Token": self.token
-            }
-        ).json()
+
         # print(data)
-        return data["children"][0]["school"]
+        return self.data_about_user["children"][0]["school"]
