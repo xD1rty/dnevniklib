6913ac6d81a8fa0402e872cbc0d57488f430effe
Иван Кривенцев <dirtyhornet277@outlook.com>
2022-12-24 20:29:02 +0300

    Изменения для PypI

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r") as fh:
 	long_description = fh.read()
 
 # Определение requests как requirements для того, чтобы этот пакет работал. Зависимости проекта.
-requirements = ["requests<=2.21.0"]
+requirements = ["requests<=2.21.0", "selenium"]
 
 # Функция, которая принимает несколько аргументов. Она присваивает эти значения пакету.
 setuptools.setup(
 	name="dnevniklib",
-	version="0.8",
+	version="1.0",
 	author="Ivan Kriventsev",
 	author_email="dirtyhornet277@outlook.com",
-	description="lib for automatization work with dnevnik mos",
+	description="Library for automated work with dnevnik.mos.ru",
 
 	long_description=long_description,
 	long_description_content_type="text/markdown",
