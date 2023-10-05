4c211770aba49494ed2cdecaec7634df490f7242
Ivan Kriventsev <dirtyhornet277@gmail.com>
2023-10-05 10:44:11 -0700

    � Изменения

---
+++ dnevniklib/homeworks/__init__.py
@@ -0,0 +1,1 @@
+from .homeworks import Homeworks
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="dnevniklib", # Replace with your username
+    version="2.0.0b",
+    author="Ivan Kriventsev",
+    author_email="<dirtyhornet277@gmail.com>",
+    description="Библиотека для работы с МЭШ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xD1rty/dnevniklib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.6',
+    install_requires=['requests', "pydantic"]
+)
